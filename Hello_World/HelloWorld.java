67653d62bbb8c96113d4689a11a6a3b3db2a3901
CMapleIII <peoriajuggalo1@gmail.com>
2016-06-26 12:37:52 -0500

    HelloWorld.java

---
+++ Hello_World/HelloWorld.java
@@ -0,0 +1,17 @@
+/** Program: Hello World
+* File: HelloWorld.java
+* Summary: Displays Hello, world
+* Author: Charles  Maple III
+* Date: June 26, 2016 
+**/
+
+public class HelloWorld 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello, world");
+
+	}
+
+}
